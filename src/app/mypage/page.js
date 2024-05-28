caea910595651914aefbfc9b80480c0d92ef5752
Oracionspace <shipfriend4371@naver.com>
2024-05-28 17:59:42 +0900

    feat: 페이지 라우팅

---
+++ src/app/mypage/page.js
@@ -0,0 +1,5 @@
+const MyPage = () => {
+  return <div>유저의 마이페이지</div>;
+};
+
+export default MyPage;
---
+++ src/app/roadmap/page.js
@@ -0,0 +1,5 @@
+const RoadMapPage = () => {
+  return <div>여기서는 로드맵을 보거나 생성할 수 있습니다.</div>;
+};
+
+export default RoadMapPage;
