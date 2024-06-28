d0be7e83cf6a927dfd3350ee10e77d80fea92841
Oracionspace <shipfriend4371@naver.com>
2024-06-28 20:42:41 +0900

    feat: navbar에 고객센터 추가

--- src/app/notice/page.js
+++ src/app/notice/page.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Page = () => {
-  return <div>{/* Your component content goes here */}</div>;
+  return (
+    <section className="maxWidth pt-32">
+      <h2 className="text-3xl">고객센터</h2>
+      <p>문제가 발생했나요?</p>
+      <div>
+        <div>공지사항 1</div>
+        <div>공지사항 2</div>
+        <div>공지사항 3</div>
+      </div>
+    </section>
+  );
 };
 
 export default Page;
