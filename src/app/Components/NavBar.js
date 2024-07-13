1be41ea256d0a42c1c9e6daf1e1bcf7bd3e349f6
JeongwooSeo <shipfriend4371@naver.com>
2024-07-13 16:05:28 +0900

    feat: 커뮤니티 link 추가

--- src/app/Components/NavBar.js
+++ src/app/Components/NavBar.js
@@ -20,7 +20,7 @@ const NavBar = () => {
         <li onClick={() => navigate.push("/")}>소개</li>
         <li onClick={() => navigate.push("/roadmap")}>로드맵</li>
         <li onClick={() => navigate.push("/faq")}>고객센터</li>
-        <li onClick={() => navigate.push("/mypage")}>마이페이지</li>
+        <li onClick={() => navigate.push("/community")}>커뮤니티</li>
         {isLoggedIn ? (
           <li onClick={() => navigate.push("/mypage")}>마이페이지</li>
         ) : (
