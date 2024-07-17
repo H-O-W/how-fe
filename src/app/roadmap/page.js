cc5e5010aa36dd85d189033a46fb6688290a1d6d
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2024-07-18 00:50:54 +0900

    fix: 페이지 이탈시 timeout 제거

--- src/app/roadmap/page.js
+++ src/app/roadmap/page.js
@@ -14,14 +14,17 @@ const Roadmap = () => {
     if (isExistRoadmap) {
     } else {
       console.log("로드맵이 없습니다! 잠시 후 생성 페이지로 이동합니다.");
-      setTimeout(() => {
+      const moveTimeout = setTimeout(() => {
         router.push("/roadmap/create");
       }, 3200);
       const timeout = setInterval(() => {
         setSecond((prev) => prev - 1);
         if (second === 0) clearInterval(timeout);
       }, 1000);
-      return () => clearInterval(timeout);
+      return () => {
+        clearTimeout(moveTimeout);
+        clearInterval(timeout);
+      };
     }
   }, [isExistRoadmap]);
 
